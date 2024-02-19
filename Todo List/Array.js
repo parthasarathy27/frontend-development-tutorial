eb6082c1e6ce31d3e6378bd185b317d1963efea5
Parthasarathy G <83574852+parthasarathy27@users.noreply.github.com>
2024-02-19 17:26:46 +0530

    Add files via upload

---
+++ Todo List/Array.js
@@ -0,0 +1,5 @@
+var fruit = ["Apple", "Banana", "Cherry", "Date", "Elderberry", "Fig", "Grape"]
+console.log(fruit.length)
+for (count = 0; count <= fruit.length-1; count++) {
+console.log(fruit[count])
+}
